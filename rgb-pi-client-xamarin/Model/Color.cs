5eca71967a7fb155112031209ec09bfbf8e3509a
Ryupold <ryupold@gmail.com>
2015-02-13 01:17:47 +0100

    testing

---
+++ rgb-pi-client-xamarin/Model/Color.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace rgbpiclientxamarin
+{
+	public struct Color
+	{
+		public float R, G, B;
+
+		public Color (float r, float g, float b)
+		{
+			R = r;
+			G = g;
+			B = b;
+		}
+
+
+	}
+}
+
---
+++ rgb-pi-client-xamarin/ViewModels/ColorChooserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace rgbpiclientxamarin
+{
+	public class ColorChooserViewModel : MvxViewModel
+	{
+		public Color CurrentColor{ get; set;}
+	}
+}
+
